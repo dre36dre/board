0a640231e5ccb50f96685566db45ca8b68fa36cb
Anderson Freires <dre.son36@gmail.com>
2025-08-22 12:37:23 -0300

    representa uma coluna dentro de um board no seu sistema Kanban.

--- Board/src/persistence/entity/BlockEntity.java
+++ Board/src/persistence/entity/BlockEntity.java
@@ -1,5 +1,18 @@
 package persistence.entity;
 
+package br.com.dio.persistence.entity;
+
+import lombok.Data;
+
+import java.time.OffsetDateTime;
+
+@Data
 public class BlockEntity {
+
+    private Long id;
+    private OffsetDateTime blockedAt;
+    private String blockReason;
+    private OffsetDateTime unblockedAt;
+    private String unblockReason;
 
 }
